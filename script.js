7786f0f781049214e652539032cbb1e10ef9d5eb
90rimane <rima.aneta@gmail.com>
2022-09-15 21:21:55 +0200

    some changes added

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-let slideIndex = 0;
+// Archive read more read less
+
+let noOfChar = 150;
+let contents = document.querySelectorAll(".content");
+//console.log(contents);
+contents.forEach(content => {
+
+  if (content.textContent.length < noOfChar)
+  {
+    content.nextElementSibling.style.display = "none";
+  }
+  else
+  {
+    let displayText = content.textContent.slice (0,noOfChar);
+    let moreText = content.textContent.slice (noOfChar);
+    console.log(contents);
+    content.innerHTML = `${displayText}<span class= "dots">...</span><span class="hide more">${moreText}</span>`;
+  }
+});
+
+ function readMore(btn){
+  let post = btn.parentElement;
+  console.log(post);
+  post.querySelector(".dots").classList.taggle("hide");
+  post.querySelector(".more").classList.taggle("hide");
+  btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
+ }
+
+ //Slide show in home
+
+ let slideIndex = 0;
 
 function showSlides() {
   let i;
