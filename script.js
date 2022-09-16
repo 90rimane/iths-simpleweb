7c666d5d099098317d0302d3f8c348f12f4184b9
90rimane <rima.aneta@gmail.com>
2022-09-16 13:18:31 +0200

    new updates added

--- script.js
+++ script.js
@@ -25,24 +25,3 @@ contents.forEach(content => {
   post.querySelector(".more").classList.taggle("hide");
   btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
  }
-
- //Slide show in home
-
- let slideIndex = 0;
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
